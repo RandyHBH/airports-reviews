e0720bcd5df684451a7d27c7e65bef69ffe67fd5
Randy Hector Bartumeu Huergo <randy.bartumeu@mytaxi.com>
2021-06-28 21:39:44 +0200

    Initial commit

---
+++ src/test/kotlin/com/example/airports/reviews/AirportsReviewsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.airports.reviews
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AirportsReviewsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
