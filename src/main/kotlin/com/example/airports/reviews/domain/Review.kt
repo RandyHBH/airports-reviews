c6929936dd98ce529e4f3e70102a1c5264bc3feb
Randy Hector Bartumeu Huergo <randy.bartumeu@mytaxi.com>
2021-06-28 21:44:22 +0200

    Add domain class Review

---
+++ src/main/kotlin/com/example/airports/reviews/domain/Review.kt
@@ -0,0 +1,54 @@
+package com.example.airports.reviews.domain
+
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+@Table(name = "review")
+data class Review(
+
+	  val airportName: String,
+
+	  val link: String,
+
+	  val title: String,
+
+	  val author: String,
+
+	  val authorCountry: String?,
+
+	  val date: LocalDate,
+
+	  val content: String,
+
+	  val experienceAirport: String?,
+
+	  val dateVisit: LocalDate?,
+
+	  val typeTraveller: String?,
+
+	  val overallRating: Double?,
+
+	  val queuingRating: Double?,
+
+	  val terminalCleanlinessRating: Double?,
+
+	  val terminalSeatingRating: Double?,
+
+	  val terminalSignsRating: Double?,
+
+	  val foodBeveragesRating: Double?,
+
+	  val airportShoppingRating: Double?,
+
+	  val wifiConnectivityRating: Double?,
+
+	  val airportStaffRating: Double?,
+
+	  val recommended: Int?
+) {
+	  @Id
+	  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "review_id_seq")
+	  @SequenceGenerator(name = "review_id_seq", sequenceName = "review_id_seq", allocationSize = 1)
+	  val id: Long? = null
+}
